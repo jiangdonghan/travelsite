45def647bf30b58086bbf1b98b0a82925faa2071
jiangdonghan <2641635339@qq.com>
2020-02-02 17:05:30 +1000

    change

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -2,12 +2,10 @@
   <div class = "wrapper">
     <swiper :options="swiperOption">
       <!-- slides -->
-      <swiper-slide>
-        <img class = "swiper-img" src="https://via.placeholder.com/600x150/0000FF">
+      <swiper-slide v-for="item of swiperList" :key = item.id>
+        <img class = "swiper-img" :src="item.imgUrl">
       </swiper-slide>
-      <swiper-slide>
-        <img class = "swiper-img" src="https://via.placeholder.com/600x150/">
-      </swiper-slide>
+
       <!-- Optional controls -->
       <div class="swiper-pagination" slot="pagination"></div>
     </swiper>
@@ -16,18 +14,32 @@
 </template>
 
 <script>
-  export default {
-    name: 'HomeSwiper',
-    data() {
-      return {
-        swiperOption: {}
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'https://via.placeholder.com/600x150/0000FF'
+      },
+      {
+        id: '0002',
+        imgUrl: 'https://via.placeholder.com/600x150/'
       }
+      ]
     }
   }
+}
 
 </script>
 
 <style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background #fff
   .wrapper
     width 100%
     overflow hidden
@@ -37,4 +49,5 @@
     // or height:31.25vw
     .swiper-img
       width 100%
+
 </style>
