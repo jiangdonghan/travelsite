598521d8063756bf311021a8ed78d928bce37d92
jiangdonghan <2641635339@qq.com>
2020-02-10 19:09:21 +1000

    add scroll function to alpahbet

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,6 +1,12 @@
 <template>
   <ul class="list">
-    <li class="item" v-for="(item,key) of alphabet" :key="key">{{key}}</li>
+    <li class="item" v-for="item of letters"
+    :key="item"
+    @click="handleLetterClick"
+    @touchstart="handleTouchStart"
+    @touchmove="handleTouchMove"
+    @touchend="handleTouchEnd"
+    :ref="item">{{item}}</li>
   </ul>
 </template>
 <script>
@@ -8,6 +14,40 @@ export default {
   name: 'CityAlphabet',
   props: {
     alphabet: Object
+  },
+  data () {
+    return {
+      touchStatus: false
+    }
+  },
+  computed: {
+    letters () {
+      const letters = []
+      for (let i in this.alphabet) {
+        letters.push(i)
+      }
+      return letters
+    }
+  },
+  methods: {
+    handleLetterClick (e) {
+      this.$emit('change', e.target.innerText)
+    },
+    handleTouchStart () {
+      this.touchStatus = true
+    },
+    handleTouchMove (e) {
+      const startY = this.$refs['A'][0].offsetTop
+      const touchY = e.touches[0].clientY - 79
+      const index = Math.floor((touchY - startY) / 20)
+      if (index >= 0 && index < this.letters.length) {
+        this.$emit('change', this.letters[index])
+      }
+      console.log(index)
+    },
+    handleTouchEnd () {
+      this.touchStatus = false
+    }
   }
 }
 </script>
