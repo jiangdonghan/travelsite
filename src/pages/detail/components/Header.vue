0b02a55fa3be00664d8301bf38b2ae51bcd697f8
jiangdonghan <2641635339@qq.com>
2020-02-19 00:34:16 +1000

    add header of detail page

---
+++ src/pages/detail/components/Header.vue
@@ -0,0 +1,74 @@
+<template>
+<div>
+  <router-link tag="div" to="/" class="header-abs" v-show="showAbs">
+    <div class="iconfont back-icon">&#xe624;</div>
+  </router-link>
+  <div class="header-fixed" v-show="!showAbs">
+    <router-link tag="div" to="/" class="iconfont header-back-icon">&#xe624;</router-link>
+    景点详情
+  </div>
+</div>
+</template>
+
+<script>
+
+export default {
+  name: 'DetailHeader',
+  data () {
+    return {
+      showAbs: true
+    }
+  },
+  methods: {
+    handleScroll () {
+      const top = document.documentElement.scrollTop
+      if (top > 60) {
+        this.showAbs = false
+      } else {
+        this.showAbs = true
+      }
+    }
+  },
+  // keep-alive so it will be used
+  activated () {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/variables.styl';
+  .header-abs
+    position: absolute
+    left: .2rem
+    top: .2rem
+    width: .8rem
+    height: .8rem
+    border-radius .4rem
+    background rgba(0,0,0,.8)
+    text-align center
+    line-height .8rem
+    .back-icon
+      color #fff
+      font-size .4rem
+  .header-fixed
+    position fixed
+    top: 0
+    left: 0
+    right: 0
+    overflow: hidden
+    height: 0.86rem
+    line-height: 0.86rem
+    text-align: center
+    color #fff
+    background: $bgColor
+    font-size .32rem
+    .header-back-icon
+      color #fff
+      position absolute
+      top 0
+      left 0
+      width .64rem
+      text-align center
+      font-size .4rem
+</style>
